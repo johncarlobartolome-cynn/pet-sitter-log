c28bb58a2c8aaf7ad191d3e2766122520406266a
John Carlo Bartolome <211265861+johncarlobartolome-cynn@users.noreply.github.com>
2026-07-18 10:42:47 +0800

    Initial commit

---
+++ test/pet-sitter-log.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PetSitterLog from '../lib/pet-sitter-log-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pet-sitter-log-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PetSitterLog.PetSitterLogStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
